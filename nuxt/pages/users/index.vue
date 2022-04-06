d06c5997bf91a1a88c68e1d08ea53c412042019c
AlekseiKromski <aleksei.kromski@outlook.com>
2022-04-06 22:10:52 +0300

    nuxtServerInit

--- nuxt/pages/users/index.vue
+++ nuxt/pages/users/index.vue
@@ -37,7 +37,8 @@ export default {
       error(e)
     }
 
-  }
+  },
+
 }
 </script>
 
--- nuxt/store/index.js
+++ nuxt/store/index.js
@@ -5,6 +5,10 @@ export const actions = {
 
   logout({commit}) {
     commit('clearToken');
+  },
+
+  async nuxtServerInit({dispatch}){
+    await dispatch('users/fetchUsers');
   }
 }
 

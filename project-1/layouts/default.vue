e67c0eb9f96d7dc15ed9a9e881dbbac214ebba25
AlekseiKromski <aleksei.kromski@outlook.com>
2022-04-06 23:08:51 +0300

    Создание Layout

---
+++ project-1/layouts/default.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="">
+
+    <el-container>
+      <el-header>
+        <app-navigation/>
+      </el-header>
+      <el-main>
+        <nuxt/>
+      </el-main>
+      <el-footer/>
+    </el-container>
+  </div>
+</template>
+
+<script>
+import AppNavigation from '~/components/main/AppNavigation';
+export default {
+  name: "default",
+  components: {
+    AppNavigation
+  }
+}
+</script>
+
+<style scoped>
+
+</style>

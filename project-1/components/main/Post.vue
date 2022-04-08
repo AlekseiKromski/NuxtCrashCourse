5f96ac44d38f4566e065229584eef3f5760ee3f2
AlekseiKromski <aleksei.kromski@outlook.com>
2022-04-08 17:26:19 +0300

    Компонент пост

--- project-1/components/main/Post.vue
+++ project-1/components/main/Post.vue
@@ -1,19 +1,74 @@
 <template>
-  <article class="post">
-    h1
-  </article>
+  <div class="post">
+    <el-card
+      shadow="hover"
+      :body-style="{padding: 0}"
+    >
+      <header
+        slot="header"
+        class="post-header"
+      >
+        <h3>Post title</h3>
+
+        <small>{{new Date().toLocaleString()}}</small>
+      </header>
+
+      <div
+        class="post-body"
+      >
+        <img
+          src="https://cms.finnair.com/resource/blob/653144/579f42b7348556e06040119aa977c330/berlin-sights-data.jpg?impolicy=crop&width=1698&height=955&x=0&y=88&imwidth=768"
+          class="post-img"
+          alt="">
+      </div>
+
+      <footer class="post-footer">
+        <el-button
+          round=""
+          @click="openPost"
+        >Открыть пост</el-button>
+
+        <span>
+        <i class="el-icon-message">12</i>
+      </span>
+      </footer>
+    </el-card>
+  </div>
+
 </template>
 
 <script>
 export default {
-  name: "Post"
+  name: "Post",
+  methods: {
+    openPost(){
+
+    }
+  }
 }
 </script>
 
 <style lang="scss" scoped>
-.post{
-  height: 200px;
-  border: 1px solid black;
-  margin: 2rem;
-}
+  .post{
+    margin-bottom: 1.5rem;
+  }
+
+  .post-header{
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  .post-image{
+    width: 100%;
+    height: auto;
+  }
+  .post-body{
+  }
+  .post-footer{
+    display: flex;
+    justify-content: space-between;
+    padding: 1rem;
+    align-items: center;
+  }
 </style>

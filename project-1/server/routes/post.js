f2406bbd916b891c068861c08bf8dc0b38430f16
AlekseiKromski <aleksei.kromski@outlook.com>
2022-04-15 20:27:34 +0300

    Контроллер постов

--- project-1/server/routes/post.js
+++ project-1/server/routes/post.js
@@ -6,39 +6,118 @@ const upload = require('../middleware/upload')
 
 //ADMIN - will be protected by Passport.js
 router.post('/admin/', [passport.authenticate('jwt'), upload.single('image')], {session: false}), async (req,res) =>{
+  const post = new Post({
+    title: req.body.title,
+    text: req.body.text,
+    imageUrl: `/${req.file.filename}`
+  });
+  try{
+    await post.save();
+    res.status(201).json(post);
+  }catch (e){
+    console.error(e);
+    res.status(500).json({"message": "server error"});
 
+  }
 })
 
 router.get('/admin/get-all', passport.authenticate('jwt', {session: false}), async (req,res) =>{
 
-})
-router.get('/admin/:id', passport.authenticate('jwt', {session: false}), async (req,res) =>{
+  try{
+    const posts = await Post.find().sort(date: -1);
+    res.status(200).json(posts);
+  }catch (e){
+    console.log(e)
+    res.status(500).json({"message": "server error"});
 
+  }
+})
+
+router.get('/admin/:id', passport.authenticate('jwt', {session: false}), async (req,res) =>{
+  try{
+    res.status(200).json(await Post.findById(req.params.id).sort(date: -1).populate('comments').exec((e, post) => {
+      res.json(post);
+    }));
+  }catch (e){
+    console.log(e)
+    res.status(500).json({"message": "server error"});
+
+  }
 })
 
 //update single post
 router.put('/admin/:id', passport.authenticate('jwt', {session: false}), async (req,res) =>{
+  try{
+    const $set = {
+      text: req.body.text
+    }
+    const post = await Post.findOneAndUpdate({
+      _id: req.params.id
+    }, {
+      $set
+    }, {
+      new: true
+    });
+    res.status(200).json(post);
+  }catch (e){
+    console.log(e)
+    res.status(500).json({"message": "server error"});
 
+  }
 })
 
 //remove single post
 router.remove('/admin/:id', passport.authenticate('jwt', {session: false}), async (req,res) =>{
+  try{
+    await Post.deleteOne({_id: req.params.id});
+    res.status(200).json({"msg": "ok"});
+  }catch (e){
+    console.log(e)
+    res.status(500).json({"message": "server error"});
 
+  }
 })
 
 //BASE
 //return all posts
 router.get('/', async (req,res) => {
+  try{
+    const posts = await Post.find().sort(date: -1);
+    res.status(200).json(posts);
+  }catch (e){
+    console.log(e)
+    res.status(500).json({"message": "server error"});
 
+  }
 })
 
 router.get('/:id', async (req,res) => {
+  try{
+    res.status(200).json(await Post.findById(req.params.id).sort(date: -1).populate('comments').exec((e, post) => {
+      res.json(post);
+    }));
+  }catch (e){
+    console.log(e)
+    res.status(500).json({"message": "server error"});
 
+  }
 })
 
 //set new view
 router.put('/:id', async (req,res) => {
+  const $set = {
+    views: ++req.body.views
+  }
 
+  try{
+    const post = await Post.findOneAndUpdate({
+      _id: req.param.id
+    },{$set});
+
+    res.status(204).json({"msg": "ok"})
+  }catch (e){
+
+  }
 })
 
 module.exports = router;

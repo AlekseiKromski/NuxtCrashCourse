adf23831daf4aaf4a86db4d89b189ee6c597f40a
AlekseiKromski <aleksei.kromski@outlook.com>
2022-04-15 19:58:02 +0300

    add multer to upload files

--- project-1/server/routes/post.js
+++ project-1/server/routes/post.js
@@ -1,9 +1,45 @@
 const router = require('express').Router();
 const passport = require('passport');
 const Post = require('../models/post');
+const upload = require('../middleware/upload')
 // /api/post
 
-router.get()
+//ADMIN - will be protected by Passport.js
+router.post('/admin/', [passport.authenticate('jwt'), upload.single('image')], {session: false}), async (req,res) =>{
+
+})
+
+router.get('/admin/get-all', passport.authenticate('jwt', {session: false}), async (req,res) =>{
+
+})
+router.get('/admin/:id', passport.authenticate('jwt', {session: false}), async (req,res) =>{
+
+})
+
+//update single post
+router.put('/admin/:id', passport.authenticate('jwt', {session: false}), async (req,res) =>{
+
+})
+
+//remove single post
+router.remove('/admin/:id', passport.authenticate('jwt', {session: false}), async (req,res) =>{
+
+})
+
+//BASE
+//return all posts
+router.get('/', async (req,res) => {
+
+})
+
+router.get('/:id', async (req,res) => {
+
+})
+
+//set new view
+router.put('/:id', async (req,res) => {
+
+})
 
 module.exports = router;
 
